8beb7c9b601b30f0db15d88188aeaa1efdf149c2
Damian Tymofijewicz <damtym06@microsoft.sci.edu.pl>
2023-10-24 21:57:16 +0200

    Initial commit and files for the Grade control.

---
+++ client/src/Components/Grade.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function Grade({ subject, attendance, grades }) {
+    subject = "Matematyka"
+    attendance = 69
+    grades = ["5", "2", "3+", "6", "4", "5", "1"]
+
+    return (
+        <div>
+            <div>
+                <p></p>
+                <img></img>
+            </div>
+            <div>
+                <p></p>
+                <div>
+                    // grades
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
