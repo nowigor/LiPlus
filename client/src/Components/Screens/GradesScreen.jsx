824b32315433cde0fd0e200634888501b61be49d
Igor_now <igornowak10@gmail.com>
2023-10-31 16:10:03 +0100

    Old version

--- client/src/Components/Screens/GradesScreen.jsx
+++ client/src/Components/Screens/GradesScreen.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-const GradesScreen = () =>
-{
-    return(
+const GradesScreen = () => {
+    return (
         <>
-        <section className="flex-wrapper">
-            <h1> GradesScreen</h1>
-        </section>
+            <section className="flex-wrapper">
+                <h1> GradesScreen</h1>
+            </section>
         </>
     )
 }
