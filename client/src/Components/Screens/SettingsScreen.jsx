e6bb3d0db67560afd0c59b1faae14befd344ab1f
Igor_now <igornowak10@gmail.com>
2023-11-01 16:58:53 +0100

    Setting screen first section

--- client/src/Components/Screens/SettingsScreen.jsx
+++ client/src/Components/Screens/SettingsScreen.jsx
@@ -1,12 +1,55 @@
 import "../../Styles/screen.css"
+import "../../Styles/SettingsScreen.css"
+import consts from "../../Constants/DesignConsts";
 import React from "react";
+import SettingsSubjectIcon from "../Icons/SettingsSubjectIcon";
 
 const SettingsScreen = () =>
-{
+{ const tmp_data = [
+    { id: "1", name: "Język Polski", grades: ["1", '2', '5+', '2+', '1'], absence: 80, average: 4.45 },
+    { id: "2", name: "Matematyka", grades: ["3", '4', '4+', '5', '3+'], absence: 30, average: 3.82 },
+    { id: "3", name: "Język Angielski", grades: ["4", '4', '4+', '5+', '4+'], absence: 100, average: 4.15 },
+    { id: "4", name: "Fizyka", grades: ["3", '4', '3+', '4+', '3+'], absence: 57, average: 3.67 },
+    { id: "5", name: "Chemia", grades: ["2", '3', '4', '3+', '2+'], absence: 95, average: 2.89 },
+    { id: "6", name: "Historia", grades: ["5", '5', '4+', '5+', '5+'], absence: 5, average: 4.95 },
+    { id: "7", name: "Biologia", grades: ["3", '4', '3+', '4', '3+'], absence: 26, average: 3.33 },
+    { id: "8", name: "Geografia", grades: ["4", '5', '4+', '5', '4+'], absence: 35, average: 4.18 },
+    { id: "9", name: "Informatyka", grades: ["2", '3', '2+', '3', '2+'], absence: 45, average: 2.55 },
+    { id: "10", name: "Sztuka", grades: ["4", '5', '4+', '5+', '4+'], absence: 30, average: 4.12 },
+    { id: "11", name: "Wychowanie Fizyczne", grades: ["3", '3+', '4', '3+', '3+'], absence: 12, average: 3.75 },
+    { id: "12", name: "Muzyka", grades: ["5", '5+', '5', '5+', '5+'], absence: 82, average: 4.95 },
+    { id: "13", name: "Etyka", grades: ["4", '4+', '4', '5', '4+'], absence: 22, average: 4.02 },
+    { id: "14", name: "Technika", grades: ["2", '3', '2+', '3', '2+'], absence: 18, average: 2.98 },
+    { id: "15", name: "Religia", grades: ["5", '5+', '5', '5+', '5+'], absence: 7, average: 5.00 },
+    { id: "16", name: "Programowanie zaawansowanych aplikacji weebowych", grades: ["3", '4', '3+', '4', '3+'], absence: 28, average: 3.47 },
+    { id: "17", name: "Psychologia", grades: ["4", '4+', '4', '5', '4+'], absence: 32, average: 4.11 },
+    { id: "18", name: "Filozofia", grades: ["3", '3+', '4', '3+', '3+'], absence: 19, average: 3.67 },
+    { id: "19", name: "Socjologia", grades: ["4", '4+', '4', '5', '4+'], absence: 27, average: 4.09 },
+    { id: "20", name: "Prawo", grades: ["2", '3', '2+', '3', '2+'], absence: 100, average: 2.75 },
+];
     return(
         <>
         <section className="flex-wrapper">
-            <h1> SettingsScreen</h1>
+            <section className="subjects-background">
+                <h1 className="text-h1-settings">Na jakie przedmioty nosisz laptopa do szkoły?</h1>
+                <div className="subjects-box">
+                {
+                    tmp_data.map((e,i)=>{
+                    const backgroundColor = consts.pastelColors[e.id - 1];
+                        return(
+                            <section  key={e.id + e.name}>
+                                <div className="single-item">
+                                    <div className="filled-circle" style={{backgroundColor: backgroundColor}}></div>
+                                    <div className="subject-text">{e.name}</div>
+                                    <SettingsSubjectIcon onClick={() => console.log(e)}/>
+                                </div>
+                                <div className="single-subject-line"></div>
+                            </section>
+                        )
+                    })
+                }
+                </div>
+            </section>
         </section>
         </>
     )
--- client/src/Constants/DesignConsts.js
+++ client/src/Constants/DesignConsts.js
@@ -1,5 +1,18 @@
 
 const COLORS = {blue: "#207CE7" , gray: "#747474" }
+const pastelColors = [
+    "#d882e7", "#d5a8c1", "#c3a8d9", "#98d4bb", "#c2de71", "#ffce7b", "#f5b041", "#c4c981", "#92a3b0", "#d1b37f",
+    "#b994d2", "#78ddce", "#b6c46e", "#cca0d0", "#df92b1", "#c9d696", "#c4c394", "#fbb170", "#e8997d", "#ce7d74",
+    "#c4aec2", "#d6e58a", "#ddc9a3", "#cf85d4", "#b2c57e", "#c7b2df", "#f0cda3", "#e0cdd5", "#fd8c7f", "#8cc0ca",
+    "#98a4c0", "#d4cfa9", "#fbb0c7", "#c6dab3", "#f8d485", "#dfaec7", "#e5bb9b", "#bd9ed0", "#a1d5ae", "#f0a9aa",
+    "#dcc2bb", "#dab28f", "#9dc3a5", "#b3c1a2", "#e2b6b2", "#8dc7b7", "#b3a9cc", "#e6c599", "#9cddab", "#d3b89a",
+    "#e6db86", "#c8e1c0", "#b0c3c3", "#f0ad95", "#d2dbab", "#efb78e", "#b0a5c7", "#fbadcf", "#ffccab", "#d4c396",
+    "#dfdd96", "#ffd89e", "#b9c6d1", "#dac6e2", "#f6d6ae", "#c5dcac", "#d5c2ce", "#dcb29b", "#ffbdc0", "#ffdf9a",
+    "#c2b9e2", "#c0daa3", "#b4b5c9", "#ddb7cc", "#dfbca0", "#c7dcb7", "#a2c0e0", "#c2df9e", "#dfd2c1", "#b1d0bf",
+    "#d6b4d4", "#c7d8a1", "#a7c9ca", "#a0c4e0", "#f8be9f", "#a2dac0", "#f3a0dd", "#f4b4bc", "#f6d5a0", "#b0d4d7",
+    "#f2d9bb", "#eccdb7", "#dcc7e0", "#e2b5d7", "#c5d6bf", "#fec8bb", "#fcccb6", "#c7c7df", "#b5bdd7", "#fed19d"
+  ];
 export default{
     COLORS,
+    pastelColors,
 }
