db1336899aae11386d1ee76c7f277fc7daa2224c
Damian Tymofijewicz <damtym06@microsoft.sci.edu.pl>
2023-11-02 15:57:54 +0100

    Improved getEvent function to include all fields given by Librus.

--- server/Utils/getEvent.js
+++ server/Utils/getEvent.js
@@ -1,37 +1,41 @@
 const cheerio = require('cheerio')
 
-function getEvent(id, isAbsence, client) {
-    if (isAbsence) {
-        return client.api._tableMapper(
-            `terminarz/szczegoly_wolne/${id}`,
-            "table.decorated.small.center tbody",
-            ["teacher", "range", "added"]
-        )
-    }
-    else {
-        const parser = data => {
-            const $ = cheerio.load(data)
-
-            const html_table = $('table.decorated.medium.center')
-            if (!html_table) {
+function getEvent(id, title, client) {
+    const parser = (data, absence) => {
+        const $ = cheerio.load(data)
+        
+        const html_table = $('table.decorated.center')
+        if (!html_table.length) {
+            if (!absence) {
+                return client._request("post",
+                    `terminarz/szczegoly_wolne/${id}`
+                ).then(d => parser(d.html(), true))
+            } else {
                 return null
             }
-
-            const table = {}
-            html_table.find('tbody > tr').each((idx, row) => {
-                const key = $(row).find("th").trim()
-                const value = $(row).find("td").trim()
-
-                table[key] = value
-            })
-            
-            return table
         }
 
-        return client._request("post",
-            `terminarz/szczegoly/${id}`
-        ).then(d => parser(d.html()))
+        const table = {}
+        html_table.find('tbody > tr').each((_, row) => {
+            const key = $(row).find('th').trim()
+            const value = $(row).find('td').trim()
+
+            if (key !== 'Zakres') {
+                table[key] = value
+            } else {
+                const [from, to] = value.split(' - ')
+                table.Od = from
+                table.Do = to
+                table.Wolne = title.slice(0, title.indexOf(':'))
+            }
+        })
+
+        return table
     }
+
+    return client._request("post",
+        `terminarz/szczegoly/${id}`
+    ).then(d => parser(d.html(), false))
 }
 
 module.exports = {
