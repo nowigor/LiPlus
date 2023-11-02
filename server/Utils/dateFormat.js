61c902ee0a5a595ee96912d47b03c93d3f943e8d
Damian Tymofijewicz <damtym06@microsoft.sci.edu.pl>
2023-11-02 15:57:26 +0100

    Added functions: returning a date in N days, calculating how many days left until a particular date, comparing which date is later.

--- server/Utils/dateFormat.js
+++ server/Utils/dateFormat.js
@@ -37,7 +37,30 @@ function _week_range(day) {
     }
 }
 
+function dateInDays(n, date = null) {
+    const today = date ? new Date(date) : new Date()
+
+    return new Date(today.getFullYear(), today.getMonth(), today.getDate() + n)
+}
+
+function daysRemaining(date) {
+    date = new Date(date)
+    const diff = Math.abs(new Date() - date)
+    
+    return Math.floor(diff / (1000 * 60 * 60 * 24))
+}
+
+function isLater(t1, t2) {
+    const date1 = new Date(`1970-01-01T${t1}:00`)
+    const date2 = new Date(`1970-01-01T${t2}:00`)
+
+    return date1 >= date2
+}
+
 module.exports = {
     weekOfDay,
-    formatDay
+    formatDay,
+    dateInDays,
+    daysRemaining,
+    isLater
 }
