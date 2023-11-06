d19a08cdac805d8e29d81fa138a60afafe8e89c9
unknown <tomsiedlecki06@gmail.com>
2023-11-06 18:18:34 +0100

    save

---
+++ client/src/Components/Counts.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import "../Styles/Counts.css"
+
+const Counts = () =>{
+    
+    return (
+        <section className="grades-box">
+            CountsScreen
+        </section>
+    )
+}
+export default Counts
--- client/src/Components/Screens/AttendanceScreen.jsx
+++ client/src/Components/Screens/AttendanceScreen.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import { ScreenSwitch } from "./ScreenSwitch";
 import { useNavigate } from "react-router-dom";
+import consts from "../../Constants/ServerConsts";
+import Counts from "../Counts";
 import "../../Styles/screen.css";
 
 const AttendaceScreen = () => {
@@ -13,9 +16,25 @@ const AttendaceScreen = () => {
         "Frekwencja": () => {}
     }
 
+    useEffect(() => {
+        getNotifcationsToday();
+    
+      }, [])
+      const [notifications, setNotifications] = useState([]);
+      const getNotifcationsToday = async () => {
+        try {
+          const response = await axios.post(consts.preURL + '/');
+          setNotifications(response.data.data);
+        } catch (error) {
+        }
+      }
+
     return (
         <section className="grades-attendance-wrapper">
             <ScreenSwitch options={["Oceny", "Frekwencja"]} active={active} setActive={setActive} onClick={onClick}/>
+            <section className="grades-attendance-box-wrapper">
+                <Counts/>
+            </section>
         </section>
     )
 }
